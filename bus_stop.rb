40db5c556668ad04bd9c9f372d84b839b874384d
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-07 15:13:15 +0100

    add_person_to_queue created

--- bus_stop.rb
+++ bus_stop.rb
@@ -1,9 +1,13 @@
 class BusStop
 
-attr_reader(:name)
+attr_reader(:name, :queue)
   def initialize(name)
     @name = name
     @queue = []
-
   end
+
+def add_person_to_queue(passenger)
+  @queue.push(passenger)
+end
+
 end
--- specs/bus_stop_specs.rb
+++ specs/bus_stop_specs.rb
@@ -7,9 +7,15 @@ class BusStopTest < MiniTest::Test
 
 def setup
   @bus_stop = BusStop.new("Lothian Road")
+  @passenger1 = Person.new("Filip", 22)
 end
 
 def test_get_bus_stop_name
   assert_equal("Lothian Road", @bus_stop.name)
+end
+
+def test_adds_person_to_queue
+  @bus_stop.add_person_to_queue(@passenger1)
+  assert_equal([@passenger1], @bus_stop.queue)
 end
 end

12c293e89129746e9ced38467f924605d8a30bba
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-07 14:21:58 +0100

    get_bus_destination created

--- specs/bus_specs.rb
+++ specs/bus_specs.rb
@@ -4,11 +4,17 @@ require_relative('../bus.rb')
 
 class BusTest < MiniTest::Test
 
-def setup
-  @bus = Bus.new(22, "Ocean Terminal")  
-end
+  def setup
+    @bus = Bus.new(22, "Ocean Terminal")
+  end
 
-def test_get_bus_number
-  assert_equal(22, @bus.number)
-end
+  def test_get_bus_number
+    assert_equal(22, @bus.number)
+  end
+
+  def test_get_bus_destination
+    assert_equal("Ocean Terminal", @bus.destination)
+  end
+
+
 end

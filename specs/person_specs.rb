f757294466c9781399cc78424eb4afac063d2267
Filip Kuszenin <filipkuszenin@gmail.com>
2018-08-07 14:32:34 +0100

    /Person class created. Get_person_name and get_person_age created

---
+++ specs/person_specs.rb
@@ -0,0 +1,19 @@
+require('minitest/autorun')
+require('minitest/rg')
+require_relative('../person.rb')
+
+class PersonTest < MiniTest::Test
+
+def setup
+  @person = Person.new("Filip",20)
+end
+
+def test_get_person_name
+  assert_equal("Filip", @person.name)
+end
+
+def test_get_person_age
+  assert_equal(20, @person.age)
+end
+
+end
